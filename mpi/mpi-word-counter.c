45b2148f8fac7674d9415d3ed819219ab3d0d9cd
PabloChristianno <pablochristianno@gmail.com>
2025-02-21 16:04:17 -0300

    cria imagem aplicacao mpi/openmp

--- mpi/mpi-word-counter.c
+++ mpi/mpi-word-counter.c
@@ -1,104 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <mpi.h>
 #include <omp.h>
+#include <time.h>
 
 #define MAX_WORD_LENGTH 100
 
-// Função para limpar a palavra: remove caracteres não alfanuméricos e converte para minúsculas
-void clean_word(char *word) {
-    int i = 0, j = 0;
-    while (word[i]) {
-        if (isalnum(word[i])) {
-            word[j++] = tolower(word[i]);
-        }
-        i++;
-    }
-    word[j] = '\0';
-}
-
-// Função para contar palavras em um arquivo
-void count_words(const char *filename, int *word_count, int *unique_words, int rank, int size) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("Erro: O arquivo '%s' não foi encontrado.\n", filename);
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    char line[1024];
+typedef struct {
     char word[MAX_WORD_LENGTH];
-    int local_word_count[1000] = {0};  // Supondo um máximo de 1000 palavras únicas por processo
-    char local_unique_words[1000][MAX_WORD_LENGTH] = {0};
-    int local_unique_count = 0;
+    int count;
+} WordCount;
 
-    while (fgets(line, sizeof(line), file)) {
-        #pragma omp parallel for private(word)
-        for (int i = 0; i < strlen(line); i++) {
-            if (isspace(line[i]) || line[i] == '\0') {
-                word[0] = '\0';
-                clean_word(word);
-                if (strlen(word) > 0) {
-                    int found = 0;
-                    #pragma omp critical
-                    {
-                        for (int j = 0; j < local_unique_count; j++) {
-                            if (strcmp(local_unique_words[j], word) == 0) {
-                                local_word_count[j]++;
-                                found = 1;
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            strcpy(local_unique_words[local_unique_count], word);
-                            local_word_count[local_unique_count]++;
-                            local_unique_count++;
-                        }
-                    }
-                }
-            } else {
-                strncat(word, &line[i], 1);
-            }
+void add_word(WordCount *words, int *size, char *word) {
+    for (int i = 0; i < *size; i++) {
+        if (strcmp(words[i].word, word) == 0) {
+            words[i].count++;
+            return;
         }
     }
-
-    fclose(file);
-
-    // Combinar resultados de todos os processos
-    MPI_Allreduce(local_word_count, word_count, 1000, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    MPI_Allgather(local_unique_words, 1000 * MAX_WORD_LENGTH, MPI_CHAR, unique_words, 1000 * MAX_WORD_LENGTH, MPI_CHAR, MPI_COMM_WORLD);
+    strcpy(words[*size].word, word);
+    words[*size].count = 1;
+    (*size)++;
 }
 
-// Função principal
+int compare_words(const void *a, const void *b) {
+    return strcmp(((WordCount *)a)->word, ((WordCount *)b)->word);
+}
+
 int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc != 2) {
-        if (rank == 0) {
-            printf("Uso: %s <nome_do_arquivo>\n", argv[0]);
-        }
-        MPI_Finalize();
-        return 1;
-    }
+    double start_time = MPI_Wtime();
 
-    int word_count[1000] = {0};
-    char unique_words[1000][MAX_WORD_LENGTH] = {0};
-
-    count_words(argv[1], word_count, (int *)unique_words, rank, size);
+    FILE *file;
+    char word[MAX_WORD_LENGTH];
+    WordCount *local_words = malloc(1000 * sizeof(WordCount));
+    int local_word_count = 0;
 
     if (rank == 0) {
-        printf("Contagem de palavras:\n");
-        for (int i = 0; i < 1000; i++) {
-            if (word_count[i] > 0) {
-                printf("%s: %d\n", unique_words[i], word_count[i]);
-            }
+        printf("Processo 0: Tentando abrir o arquivo palavras.txt...\n");
+        fflush(stdout);
+        file = fopen("/app/palavras.txt", "r");
+        if (file == NULL) {
+            perror("Erro ao abrir o arquivo");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        } else {
+            printf("Arquivo palavras.txt aberto com sucesso!\n");
+            fflush(stdout);
         }
     }
 
+    while (1) {
+        if (rank == 0) {
+            if (fscanf(file, "%s", word) != EOF) {
+                for (int i = 1; i < size; i++) {
+                    MPI_Send(word, MAX_WORD_LENGTH, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+                }
+            } else {
+                for (int i = 1; i < size; i++) {
+                    MPI_Send("", MAX_WORD_LENGTH, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+                }
+                break;
+            }
+        } else {
+            MPI_Recv(word, MAX_WORD_LENGTH, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (strlen(word) == 0) break;
+        }
+
+        #pragma omp parallel for
+        for (int i = 0; i < 1; i++) {
+            add_word(local_words, &local_word_count, word);
+        }
+    }
+
+    if (rank == 0) {
+        fclose(file);
+    }
+
+    WordCount *global_words = NULL;
+    int global_word_count = 0;
+
+    if (rank == 0) {
+        global_words = malloc(1000 * size * sizeof(WordCount));
+    }
+
+    MPI_Gather(local_words, local_word_count * sizeof(WordCount), MPI_BYTE,
+               global_words, local_word_count * sizeof(WordCount), MPI_BYTE,
+               0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < local_word_count; j++) {
+                add_word(global_words, &global_word_count, global_words[i * local_word_count + j].word);
+            }
+        }
+
+        qsort(global_words, global_word_count, sizeof(WordCount), compare_words);
+
+        printf("Contagem de palavras:\n");
+        fflush(stdout);
+        for (int i = 0; i < global_word_count; i++) {
+            printf("%s: %d\n", global_words[i].word, global_words[i].count);
+            fflush(stdout);
+        }
+
+        double end_time = MPI_Wtime();
+        printf("Tempo de execução: %f segundos\n", end_time - start_time);
+        fflush(stdout);
+
+        free(global_words);
+    }
+
+    free(local_words);
     MPI_Finalize();
     return 0;
 }
